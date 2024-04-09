05a99f4c2470b2f3f5d1bd3d3f59084c6e11e307
wekesamuel <samuelsweke@gmail.com>
2024-04-09 14:24:08 +0100

    login dark mode fix

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SignUp() {
   return (
-    <main className="bg-gray-50 dark:bg-gray-900">
+    <main className="bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a
           href="#"
@@ -10,9 +10,9 @@ export default function SignUp() {
         >
           NarrativeNest
         </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-xl text-center font-bold leading-tight tracking-tight md:text-2xl text-white">
               Create an account
             </h1>
             <form className="space-y-4 md:space-y-6">
@@ -86,14 +86,14 @@ export default function SignUp() {
               <div>
                 <label
                   htmlFor="countries"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-white"
                 >
                   Role
                 </label>
                 <select
                   id="countries"
                   required
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                 >
                   <option selected>Choose a role</option>
                   <option value="student">Student</option>
@@ -108,13 +108,13 @@ export default function SignUp() {
                 type="submit"
                 className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
-                Sign Up
+                Create account
               </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              <p className="text-sm font-light text-gray-400">
                 Already have an account?{" "}
                 <a
                   href="#"
-                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  className="font-medium hover:underline text-primary-500"
                 >
                   Login
                 </a>
@@ -126,3 +126,4 @@ export default function SignUp() {
     </main>
   );
 }
+
